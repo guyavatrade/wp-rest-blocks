ecda7c20e990eee491b862d7aa36efdaeaa853a4
Guy Cohen <g.cohen@avatrade.com>
2025-09-03 10:30:23 +0000

    add innerHTML attribute to core/html

--- src/ava/remove-rest-api-fields.php
+++ src/ava/remove-rest-api-fields.php
@@ -2,8 +2,10 @@
 function remove_rest_api_fields(&$block, &$attr)
 {
   // remove $block['innerContent'] and $block['innerHTML'] fields
+  if ($block["blockName"] !== "core/html") {
+    unset($block['innerHTML']);
+  }
   unset($block['innerContent']);
-  unset($block['innerHTML']);
 
   // loop over $attr and remove empty fields
   foreach ($attr as $key => $value) {
